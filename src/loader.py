9d442479bfa28ff96437192fed28dccdf5fd703c
손정균_T3111 <79796061+Rimo-rimo@users.noreply.github.com>
2022-02-23 15:10:33 +0900

    Update loader.py

---
+++ src/loader.py
@@ -0,0 +1,37 @@
+import os
+import cv2
+import pandas as pd
+import numpy as np
+import easydict
+import albumentations as A
+from albumentations.pytorch import ToTensorV2
+import torch
+from torch.utils.data import Dataset, DataLoader
+
+class Dataset(Dataset):
+    def __init__(self, df, train=True, transform=None):
+        
+        self.df = df
+        self.train = train
+        if self.train:
+            self.img_path = df["img_path"]
+            self.label = df["label"]
+        else:
+            self.img_path = df["img_path"]
+        self.transform = transform
+    
+    def __getitem__(self, idx):
+        
+        image = cv2.imread(self.img_path[idx])
+        image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+        
+        if self.transform:
+            image = self.transform(image=image)["image"]
+        if self.train:
+            label = self.label[idx]
+            return image, label
+        else:
+            return image
+    
+    def __len__(self):
+        return len(self.df)
