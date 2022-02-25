a2efe29f048bff53a559bd0cece9f74ec92098c3
ddrongy <ddrongy@gmail.com>
2022-02-25 11:36:58 +0900

    Update models.py

--- src/models.py
+++ src/models.py
@@ -12,3 +12,41 @@ class pretrainedModel(nn.Module):
         x=self.net()
 
         return x
+    
+    
+class agegenderModel(nn.Module):
+    def __init__(self, encoder, encoder_channels, 
+                 age_classes, gender_classes, output_channels=512):
+        super().__init__()
+        
+        # encoder features (resnet50 in my case)
+        # output should be bs x c x h x w
+        self.encoder = encoder
+        
+        # sqeeze-excite
+        self.squeeze = nn.AdaptiveAvgPool2d(1)
+        self.downsample = nn.Conv2d(encoder_channels, output_channels, 1)
+        self.bn1 = nn.BatchNorm2d(output_channels)
+        self.nonlin1 = nn.ReLU()
+        
+        self.excite = nn.Conv2d(output_channels, output_channels, 1)
+        self.bn2 = nn.BatchNorm2d(output_channels)
+        self.nonlin2 = nn.ReLU()
+        
+        self.age_head = nn.Conv2d(output_channels, age_classes, 1)
+        self.gender_head = nn.Conv2d(output_channels, gender_classes, 1)
+    
+    def forward(self, x):
+        features = self.encoder(x)
+        features = self.squeeze(features)
+        features = self.downsample(features)
+        features = self.nonlin1(self.bn1(features))
+        
+        weights_logits = self.excite(features)
+        features = features * weights_logits.sigmoid()
+        features = self.nonlin2(self.bn2(features))
+        
+        age_logits = self.age_head(features).view(features.size(0), -1)
+        gender_logits = self.gender_head(features).view(features.size(0), -1)
+        total_logits = gender_logits * 3 + age_logits
+        return total_logits
