ced84e3fc272ccc0a132f59f6d20bd4e734f4f71
TSTP <86605720+seokjin0404@users.noreply.github.com>
2022-02-27 23:16:26 +0900

    Create inference_skf.py

---
+++ base/inference_skf.py
@@ -0,0 +1,96 @@
+import argparse
+import os
+from importlib import import_module
+import numpy as np
+import pandas as pd
+import torch
+from torch.utils.data import DataLoader
+from scipy import stats
+from dataset import TestDataset, MaskBaseDataset
+
+
+def load_model(saved_model, num_classes, device, i):
+    model_cls = getattr(import_module("model"), args.model)
+    model = model_cls(
+        num_classes=num_classes
+    )
+
+    # tarpath = os.path.join(saved_model, 'best.tar.gz')
+    # tar = tarfile.open(tarpath, 'r:gz')
+    # tar.extractall(path=saved_model)
+    saved_model = saved_model + f'/{i}'
+    model_path = os.path.join(saved_model, 'best.pth')
+    model.load_state_dict(torch.load(model_path, map_location=device))
+
+    return model
+
+
+@torch.no_grad()
+def inference(data_dir, model_dir, output_dir, args):
+    """
+    """
+    use_cuda = torch.cuda.is_available()
+    device = torch.device("cuda" if use_cuda else "cpu")
+
+    num_classes = MaskBaseDataset.num_classes  # 18
+    pred_total = np.empty((1,12600))
+    for i in range(5):
+        model = load_model(model_dir, num_classes, device, i).to(device)
+        model.eval()
+
+        img_root = os.path.join(data_dir, 'images')
+        info_path = os.path.join(data_dir, 'info.csv')
+        info = pd.read_csv(info_path)
+
+        img_paths = [os.path.join(img_root, img_id) for img_id in info.ImageID]
+        dataset = TestDataset(img_paths, args.resize)
+        loader = torch.utils.data.DataLoader(
+            dataset,
+            batch_size=args.batch_size,
+            num_workers=8,
+            shuffle=False,
+            pin_memory=use_cuda,
+            drop_last=False,
+        )
+
+        print("Calculating inference results..")
+        preds = np.array([])
+        with torch.no_grad():
+            for idx, images in enumerate(loader):
+                images = images.to(device)
+                pred = model(images)
+                pred = pred.argmax(dim = -1)
+                preds = np.append(preds,pred.cpu().numpy())
+        pred_total = np.append(pred_total,[preds], axis = 0)
+    pred_total = np.delete(pred_total,(0), axis = 0)
+    print(pred_total.shape)
+    pred_total = stats.mode(pred_total)
+
+
+    info['ans'] = pred_total[0].squeeze(axis = 0).astype(np.int)
+    info.to_csv(os.path.join(output_dir, f'output.csv'), index=False)
+    print(f'Inference Done!')
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+
+    # Data and model checkpoints directories
+    parser.add_argument('--batch_size', type=int, default=1000, help='input batch size for validing (default: 1000)')
+    parser.add_argument('--resize', type=tuple, default=(128, 96), help='resize size for image when you trained (default: (96, 128))')
+    parser.add_argument('--model', type=str, default='BaseModel', help='model type (default: BaseModel)')
+
+    # Container environment
+    parser.add_argument('--data_dir', type=str, default=os.environ.get('SM_CHANNEL_EVAL', '/opt/ml/input/data/eval'))
+    parser.add_argument('--model_dir', type=str, default=os.environ.get('SM_CHANNEL_MODEL', './model'))
+    parser.add_argument('--output_dir', type=str, default=os.environ.get('SM_OUTPUT_DATA_DIR', './output'))
+
+    args = parser.parse_args()
+
+    data_dir = args.data_dir
+    model_dir = args.model_dir
+    output_dir = args.output_dir
+
+    os.makedirs(output_dir, exist_ok=True)
+
+    inference(data_dir, model_dir, output_dir, args)
